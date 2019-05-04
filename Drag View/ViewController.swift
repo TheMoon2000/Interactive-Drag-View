8ecd82cdd62b01e70c8362c7226efbad1e65e041
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-05-04 15:41:58 -0700

    Initial Commit

---
+++ Drag View/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Drag View
+//
+//  Created by Jia Rui Shan on 2019/5/4.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
